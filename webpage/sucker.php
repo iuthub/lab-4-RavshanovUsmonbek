52c1b4c0c2edd87d1b2fa4b4481a23db25cca8b8
RavshanovUsmonbek <36451884+RavshanovUsmonbek@users.noreply.github.com>
2018-05-04 00:01:38 +0500

    Exercise 4

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -4,12 +4,55 @@
 		<title>Buy Your Way to a Better Education!</title>
 		<link href="buyagrade.css" type="text/css" rel="stylesheet" />
 	</head>
-
 	<body>
+		<?php
+		function converter($string)
+		{
+			if($string==="name")
+				return "Name";
+			elseif ($string==="section")
+				return "Section";
+			elseif ($string==="credit_card_number")
+				return "Credit card number";
+			elseif ($string==="credit_card_type")
+				return "Credit card type";
+		}
+
+		$arrErr=array();
+		$count=0;
+			foreach ($_POST as $key => $value) {
+				if(!$value)
+				{
+					$count++;
+					array_push($arrErr, converter($key));
+				}
+			}
+			if($count==count($_POST)-1)
+			{
+
+				?>
+				<h1>Sorry</h1>
+
+				<p>You did not fill out any of the fields.<a href="buyagrade.html">Try again</a></p>
+	
+	<?php	
+			}
+			
+			elseif ($count>0)
+			 {
+				echo "<h1>Sorry</h1> You did not fill out these fields:<br/>";
+				foreach ($arrErr as $err) {
+					echo "$err<br/>";
+				}
+					if(!in_array("MasterCard", $_POST) && !in_array("visa", $_POST)) echo "Credit card type<br/>";
+			 }
+			else
+			{
+
+		?>
 		<h1>Thanks, sucker!</h1>
 
 		<p>Your information has been recorded.</p>
-
 		<dl>
 			<dt>Name</dt>
 			<dd><?= $_POST['name']?></dd>
@@ -18,10 +61,11 @@
 			<dd><?= $_POST["section"]  ?></dd>
 
 			<dt>Credit Card</dt>
-			<dd><?= $_POST["credit_card_number"]?> (<?= $_POST["credit_card"]?>)</dd>
+			<dd><?= $_POST["credit_card_number"]?> (<?= $_POST["credit_card_type"]?>)</dd>
 		</dl>
 		<?php 
-			 $str=trim(implode(";", $_POST),";I am a giant sucker.")."<br/>";
+			 array_pop($_POST);
+			 $str=implode(";", $_POST)."<br/>";
 			 file_put_contents("suckers.txt", $str,FILE_APPEND);
 		?>
 		Here are all suckers who have submitted here:
@@ -31,7 +75,7 @@
 			 echo "<br/>";
 			?>
 		</p>
-
+<?php } ?>
 
 	</body>
 </html>  
